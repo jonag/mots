00e894d5a870dbef3a3c78ba4980e2c71ddd0efd
Jonathan GONCALVES <jgoncalves@a5sys.com>
2020-03-17 12:37:28 +0100

    test ws

--- server.js
+++ server.js
@@ -1,61 +1,64 @@
 /**
  * Module dependencies.
  */
-var express = require('express'),
+var /*express = require('express'),
     routes  = require('./routes'),
     http  = require('http'),
     path  = require('path'),
     os = require('os'),
     prompts = require('prompts'),
     app   = express(),
-    config  = require('./conf.json'),
+    config  = require('./conf.json'),*/
     mfl   = require('./game_files/motsFleches'),
 
     _gridNumber = 0;
 
 
-// all environments
-app.set('port', config.SERVER_PORT);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+mfl.startMflServer(_gridNumber);
 
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/conf.json', function(req, res) {
-    res.json({ SOCKET_ADDR: config.SOCKET_ADDR, SOCKET_PORT: config.SOCKET_PORT });
-});
-
-// Start server
-http.createServer(app).listen(app.get('port'), onServerReady);
-
-// Retreive command line arguments
-if (process.argv[2]) {
-  // If the user wants the default grid (debug purpose)
-  if ((isNaN(process.argv[2])) && (process.argv[2].toLowerCase() == 'default'))
-    _gridNumber = -1;
-  // Else if the user try to retreive a special grid
-  else if (!isNaN(process.argv[2]))
-    _gridNumber = process.argv[2];
-}
-
-/** Call when the express server has started */
-async function onServerReady() {
-  console.log('Express server listening on port ' + app.get('port'));
-  console.log(`\n\n\tWaiting for players at ${config.SOCKET_ADDR}:${config.SERVER_PORT}\n\n`);
-
-  // Load desired grid in parameter.
-  // -1 to retreive the day grid, 0 for the default one or any number for a special one
-  mfl.startMflServer(_gridNumber);
-}
+// // all environments
+// app.set('port', config.SERVER_PORT);
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+//
+// app.use(express.favicon());
+// app.use(express.logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.methodOverride());
+// app.use(app.router);
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// // development only
+// if ('development' == app.get('env')) {
+//   app.use(express.errorHandler());
+// }
+//
+// app.get('/', routes.index);
+// app.get('/conf.json', function(req, res) {
+//     res.json({ SOCKET_ADDR: config.SOCKET_ADDR, SOCKET_PORT: config.SOCKET_PORT });
+// });
+//
+// // Start server
+// http.createServer(app).listen(app.get('port'), onServerReady);
+//
+// // Retreive command line arguments
+// if (process.argv[2]) {
+//   // If the user wants the default grid (debug purpose)
+//   if ((isNaN(process.argv[2])) && (process.argv[2].toLowerCase() == 'default'))
+//     _gridNumber = -1;
+//   // Else if the user try to retreive a special grid
+//   else if (!isNaN(process.argv[2]))
+//     _gridNumber = process.argv[2];
+// }
+//
+// /** Call when the express server has started */
+// async function onServerReady() {
+//   console.log('Express server listening on port ' + app.get('port'));
+//   console.log(`\n\n\tWaiting for players at ${config.SOCKET_ADDR}:${config.SERVER_PORT}\n\n`);
+//
+//   // Load desired grid in parameter.
+//   // -1 to retreive the day grid, 0 for the default one or any number for a special one
+//   mfl.startMflServer(_gridNumber);
+// }
